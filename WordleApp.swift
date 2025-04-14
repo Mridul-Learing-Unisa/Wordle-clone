5d60f1e1e612cb42c6e4548f65c0f1bb7d48e89b
mridulchopr23 <chomy077@mymail.unisa.edu.au>
2025-04-14 17:06:09 +0930

    Initial commit

---
+++ WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by mri on 14/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
